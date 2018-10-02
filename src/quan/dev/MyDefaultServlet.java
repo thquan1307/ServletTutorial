ee34af6fd6dff97e5ff7de596b737cf36852e85a
TranHongQuan <quantran.130798@gmail.com>
2018-10-02 22:12:54 +0700

    Finish MyDefaultServlet

---
+++ src/quan/dev/MyDefaultServlet.java
@@ -0,0 +1,50 @@
+package quan.dev;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyDefaultServlet
+ */
+@WebServlet(urlPatterns = { "/" })
+public class MyDefaultServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyDefaultServlet() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletOutputStream out = response.getOutputStream();
+		 
+	       out.println("<html>");
+	       out.println("<head><title>Page not found</title></head>");
+	 
+	       out.println("<body>");
+	       out.println("<h3>Sorry! Page not found</h3>");
+	       out.println("<h1>404</h1>");
+	       out.println("Message from servlet: " + this.getClass().getName());
+	       out.println("</body>");
+	       out.println("<html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
